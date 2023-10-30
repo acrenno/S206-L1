5b0b2662e17093f414fde49034b9bc1c474aafba
annac@NITRO_ACR <annaclararenno@gmail.com>
2023-10-30 20:42:07 -0300

    Exercicio para aula 3

--- testes/cypress/e2e/exemplo1.cy.js
+++ testes/cypress/e2e/exemplo1.cy.js
@@ -5,14 +5,15 @@ describe('Criando cenario de teste para o site globalsqa',()=>{
   it.skip ('Caso de teste: Registrar um usuario no site com sucesso', ()=>{
     cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login')
     cy.get('.btn-link').click()
-    cy.get('#firstName').type('inatel')
-    cy.get('#TEST1').type('inatel')
-    cy.get('#username').type('inatel')
-    cy.get('#password').type('inatel')
+    cy.get('#firstName').type('anna')
+    cy.get('#TEST1').type('anna')
+    cy.get('#username').type('anna')
+    cy.get('#password').type('aninha')
     cy.get('.btn-primary').click()
     cy.get('.ng-binding').should('contain.text', 'Registration successful')
   })
 
+  /*
   it.skip ('Caso de teste: Registrar um usuario no site sem sucesso', ()=>{
     cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/register')
   
@@ -25,6 +26,7 @@ describe('Criando cenario de teste para o site globalsqa',()=>{
     cy.get('.btn-primary').should('be.disabled')
    
   })
+  */
 
   it ('Caso de teste: Registrar um usuario no site com sucesso', ()=>{
    [
@@ -40,9 +42,8 @@ function criarusuario(){
 
 let horas = new Date().getHours().toString()
 let minutos = new Date().getMinutes().toString()
-let seg = new Date().getSeconds().toString()
-let user = horas + minutos + seg + 'ID'
-let senha = horas + minutos + seg + 'senha'
+let user = horas + minutos + 'aninha'
+let senha = horas + minutos + 'senha'
 let usuarioInfo = [user,senha]
 
   cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login')
